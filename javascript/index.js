ae16e4e3316fcdf6470194687527447fc89db367
GaliNeufeld <101136202+GaliNeufeld@users.noreply.github.com>
2022-12-06 17:09:50 -0500

    Added function to continuously update time in all cities

--- javascript/index.js
+++ javascript/index.js
@@ -31,25 +31,28 @@ let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
 
 function updateCity(event) {
-  let cityTimeZone = event.target.value;
-  if (cityTimeZone === "current") {
-    cityTimeZone = moment.tz.guess();  
-  }
-  let cityName = cityTimeZone
-    .replace("_", " ")
-    .replace("Tokyo", "Osaka")
-    .split("/")[1];
-  let cityTime = moment().tz(cityTimeZone);
-  let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = ` 
+   setInterval(function () {
+     let cityTimeZone = event.target.value;
+     if (cityTimeZone === "current") {
+       cityTimeZone = moment.tz.guess();
+     }
+     let cityName = cityTimeZone
+       .replace("_", " ")
+       .replace("Tokyo", "Osaka")
+       .split("/")[1];
+     let cityTime = moment().tz(cityTimeZone);
+     let citiesElement = document.querySelector("#cities");
+     citiesElement.innerHTML = ` 
   <div class="city">
         <div>
           <h2>${cityName}</h2>
           <div class="date">${cityTime.format("MMMM D, YYYY")}</div>
         </div>
         <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
-    "A"
-  )}</small>
+       "A"
+     )}</small>
         </div>
       </div>`;
+   }, 1000);   
 }
+
